240232b2206bee5492f650a009510ea93bdb4ec8
Diyar <diyarbarham@gmail.com>
2023-03-15 11:57:26 +0200

    answerd Valid Anagram question in leetcode

--- LeetCode/ContainsDuplicate/ContainsDuplicate/Program.cs
+++ LeetCode/ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -1,9 +1,18 @@
 ﻿namespace ContainsDuplicate;
-class Program
+class Program // problem url https://leetcode.com/problems/contains-duplicate/
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+    }
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> vals = new HashSet<int>();
+        foreach (int n in nums){
+            if (vals.Contains(n)) return true;
+            vals.Add(n);
+        }
+        return false;
     }
 }
 
---
+++ LeetCode/ValidAnagram/ValidAnagram/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ValidAnagram;
+class Program //Prorblem URL https://leetcode.com/problems/valid-anagram/
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"{IsAnagram("aacc", "ccac")}");
+    }
+    public static bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+        if (s == t) return true;
+
+        foreach(char indexer in s)
+        {
+            if (t.IndexOf(indexer) == -1) return false;
+            Console.WriteLine($"{t.IndexOf(indexer)}");
+            t = t.Remove(t.IndexOf(indexer), 1);
+            Console.WriteLine($"{t}");
+        }
+
+        return true;
+    }
+}
+
