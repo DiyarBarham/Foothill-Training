65e87f983a1a9f95fd402a2e769712bfae31a9d1
Diyar <diyarbarham@gmail.com>
2023-03-15 12:41:51 +0200

    Answering Two Sum question in LeetCode

---
+++ LeetCode/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TwoSum;
+class Program // Question URL https://leetcode.com/problems/two-sum/description/
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        for(int i =0; i< nums.Length; i++)
+        {
+            for(int j=i+1; j< nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target) return new int[] { i, j }; ;
+            }
+            
+        }
+        return null;
+    }
+}
+
