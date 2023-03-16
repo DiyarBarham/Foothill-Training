1064b14f14873fc32aadfe40d3e76c712396b6a4
Diyar <diyarbarham@gmail.com>
2023-03-16 11:04:19 +0200

    adding the product class

---
+++ ACM/ACM.BL/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ACM.BL
+{
+	public class Product
+	{
+        public decimal? currentPrice { get; set; }
+        public String productDescription { get; set; }
+        public String productName { get; set; }
+        public int productID { get; private set; }
+
+        public Product()
+		{
+		}
+
+        public Product(int productID)
+        {
+            this.productID = productID;
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if (String.IsNullOrWhiteSpace(productName)) isValid = false;
+            if (currentPrice == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
+
