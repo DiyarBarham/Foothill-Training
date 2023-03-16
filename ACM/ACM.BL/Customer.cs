0c90196da79f8a3c91e44e209467144b90c6f42f
Diyar <diyarbarham@gmail.com>
2023-03-16 10:43:55 +0200

    testing for customer class

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace ACM.BL;
+public class Customer
+{
+    public String emailAddress { get; set; }
+    public int customerID{ get; private set; }
+    public String firstName { get; set; }
+    public String lastName { get; set; }
+    public String fullName { get {
+            return firstName + ", " + lastName;} }
+     
+}
+
---
+++ ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,24 @@
+using ACM.BL;
+namespace ACM.BLTest;
+
+[TestClass]
+public class CustomerTest
+{
+    [TestMethod]
+    public void FullNameTestValid ()
+    {
+        //Arrange
+        Customer customer = new Customer
+        {
+            firstName = "Mohammad",
+            lastName = "Ali"
+        };
+        String expected = "Mohammad, Ali";
+
+        //Act
+        String result = customer.fullName;
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+} 
