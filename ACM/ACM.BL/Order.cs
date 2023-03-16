dd5da2d67c6c88cef606e569de5d5a9ef1b5fd09
Diyar <diyarbarham@gmail.com>
2023-03-16 11:09:02 +0200

    building the order Class

---
+++ ACM/ACM.BL/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ACM.BL
+{
+	public class Order
+	{
+		public DateTimeOffset? orderDate { get; set; }
+		public int orderID { get; private set; }
+
+		public Order()
+		{
+		}
+
+        public Order(int orderID)
+        {
+			this.orderID = orderID;
+        }
+
+		public Order Retrieve(int orderID)
+		{
+			return new Order();
+		}
+
+		public bool Save()
+		{
+			return true;
+		}
+
+		public bool Validate()
+		{
+			bool isValid = true;
+
+			if (orderDate == null) isValid = false;
+
+			return isValid;
+		}
+    }
+}
+
