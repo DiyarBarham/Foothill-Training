4f57d734a59da143985d1ab3c19ec06795f1e0f1
Diyar <diyarbarham@gmail.com>
2023-03-19 16:50:06 +0200

    Answering TopKFrequentElements LeetCode Problem

---
+++ LeetCode/TopKFrequentElements/TopKFrequentElements/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace TopKFrequentElements;
+class Program //Leet Code URL https://leetcode.com/problems/top-k-frequent-elements/
+{
+    static void Main(string[] args)
+    { int[] inp = { 1, 1, 1, 2, 2, 3 };
+        int[] result = TopKFrequent(inp,2);
+        foreach(int i in result)
+        {
+            Console.WriteLine($"{i}");
+        }
+    }
+
+    public static int[] TopKFrequent(int[] nums, int k)
+    {
+        int[] result = new int[k];
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(nums[i]))
+            {
+                dict[nums[i]]++;
+            }
+            else
+            {
+                dict.Add(nums[i], 1);
+            }
+        }
+        var pq = new PriorityQueue<int, int>();
+        foreach (var key in dict.Keys)
+        {
+            pq.Enqueue(key, dict[key]);
+            if (pq.Count > k) pq.Dequeue();
+        }
+        for(int i=k-1; i>=0; i--)
+        {
+            result[i] = pq.Dequeue();
+        }
+        return result;
+    }
+}
+
